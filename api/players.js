c4d5a2879886e348655b31d7cfdc73baae19d41d
Eghizio <eghizio@gmail.com>
2020-05-12 02:50:54 +0200

    Implemented simple Cache for /players

--- api/players.js
+++ api/players.js
@@ -2,13 +2,25 @@
 const express = require("express");
 const axios = require("axios");
 const { Parser } = require("xml2js");
-
+const Cache = require("../utils/Cache");
 
 const router = express.Router();
+const cache = new Cache();
 
 router.get("/", (req, res) => {
+    // Need to move logs to cache, prolly will swap to use node-cache behind the Cache class
+    // Need to create services and extract the logic
+    console.log(`Calling "${req.baseUrl}"...`);
+
+    if(cache.has(req.baseUrl)){
+        console.log(`Retrieving "${req.baseUrl}" from cache...`);
+        return res.json(cache.get(req.baseUrl));
+    }
+
+    console.log(`No cache item found for ${req.baseUrl}`);
+
     const XML_Parser = new Parser();
-    
+
     axios.get(req.app.get("ogameAPI").players)
         .then(response => response.data)
         .then(xml => 
@@ -24,6 +36,7 @@ router.get("/", (req, res) => {
 
             return orderedJSON;
         })
+        .then(data => cache.set(req.baseUrl, data))
         .then(formatedJSON => res.json(formatedJSON))
         .catch(err => res.send(err));
 });
