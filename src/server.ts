c80d3bc3760932ab51cbc75a0002308689d8e929
Eghizio <eghizio@gmail.com>
2021-09-25 00:13:27 +0200

    Updated cors origin

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { api } from "./routes/api";
 export const createServer = () => {
     const app = express();
 
-    app.use(cors());
+    app.use(cors({ origin: "*" }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     
