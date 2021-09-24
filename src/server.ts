d0b344b36d2761ddd6bca8718282e0a3ca8c32df
Eghizio <eghizio@gmail.com>
2021-09-25 00:05:40 +0200

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { api } from "./routes/api";
 
 export const createServer = () => {
     const app = express();
 
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     
